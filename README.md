2a71b8a9232346120378050f0e9baacdf1b2cdc0
Patrick Stalcup <legomaniac0117@gmail.com>
2022-12-02 09:33:46 -0500

    Add progress tracking mode

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # Advent of Code 2022
 
 [Advent of Code](https://adventofcode.com/) is a programming challenge that is run annually. These are my soltuions to the 2022 competition.
+
+
+## Running
+
+To run:
+
+```bash
+python3 aoc.py dayN ... 
+```
+
+Where N is a number (1 based) representing the day of the puzzle.
+
+`solutions/dayN.py` is expected to define functions `part1` and `part2`, each taking a string as an argument.
+
+When called, these functions should return the solution for that days part 1 and part 2 respectively.
+
+
+## Tracking Progress
+
+To track progress:
+
+```bash
+python3 aoc.py solved dayN ... 
+```
+
+Where N is a number (1 based) representing the day of the puzzle. This will update the `progress.txt` file with all days that have solutions.
--- aoc.py
+++ aoc.py
@@ -1,20 +1,42 @@
 import sys, re, os
 import solutions
 
+def base():
+    return os.path.dirname(__file__)
+
 if __name__ == "__main__":
     day_regex = re.compile("day\d")
     days = [p for p in sys.argv if day_regex.match(p)]
+
+    mode = "run"
+    for m in sys.argv:
+        if m == "solved":
+            mode = "solved"
 
     parts = ["part1", "part2"]
 
     for day in days:
         print("----")
         print(day)
-        with open(os.path.join(os.path.dirname(__file__), "inputs", f"{day}.txt")) as f:
-            s = f.read()
 
-        __import__(f"solutions.{day}")
-        mod = getattr(solutions, day)
-        for part in parts:
-            if hasattr(mod, part):
-                print(f"{part}: {getattr(mod, part)(s)}")
+        if mode == "run":
+            with open(os.path.join(base(), "inputs", f"{day}.txt")) as f:
+                s = f.read()
+
+            __import__(f"solutions.{day}")
+            mod = getattr(solutions, day)
+            for part in parts:
+                if hasattr(mod, part):
+                    print(f"{part}: {getattr(mod, part)(s)}")
+        elif mode == "solved":
+            progress_path = os.path.join(base(), "progress.txt")
+            if(os.path.exists(progress_path)):
+                with open(progress_path, "r") as f:
+                    all_solved = list(set(f.read().split("\n")[:-1] + [day]))
+            else:
+                all_solved = [day]
+            
+            with open(progress_path, "w") as f:
+                for s in sorted(all_solved, key=lambda x: int(x.replace("day", ""))):
+                    f.write(f"{s}\n")
+                
